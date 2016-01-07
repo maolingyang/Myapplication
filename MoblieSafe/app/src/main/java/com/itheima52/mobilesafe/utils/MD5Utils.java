8bee553f3f1d3e6e729022d3d21f5f633b154b0c
maolingyang <854717818@qq.com>
2016-01-07 20:47:45 +0800

    static

--- MoblieSafe/app/src/main/java/com/itheima52/mobilesafe/utils/MD5Utils.java
+++ MoblieSafe/app/src/main/java/com/itheima52/mobilesafe/utils/MD5Utils.java
@@ -70,4 +70,55 @@ public class MD5Utils {
 			return "";
 		}
 	}
+
+
+	/**
+	 * 获取到文件的MD5(病毒特征码)
+	 * @param sourceDir
+	 * @return
+	 * 主动防御
+	 */
+	public static String getFileMd52(String sourceDir) {
+
+		File file = new File(sourceDir);
+
+		try {
+			FileInputStream fis = new FileInputStream(file);
+
+			byte[] buffer =  new byte[1024];
+
+			int len = -1;
+			//获取到数字摘要
+			MessageDigest messageDigest = MessageDigest.getInstance("md5");
+
+			while((len = fis.read(buffer))!= -1){
+
+				messageDigest.update(buffer, 0, len);
+
+			}
+			byte[] result = messageDigest.digest();
+
+			StringBuffer sb = new StringBuffer();
+
+			for(byte b : result){
+				int number = b&0xff; // 加盐 +1 ;
+				String hex = Integer.toHexString(number);
+				if(hex.length()==1){
+					sb.append("0"+hex);
+				}else{
+					sb.append(hex);
+				}
+			}
+			return sb.toString();
+
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		return null;
+	}
+
 }
